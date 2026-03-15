48c2ace62f50e24f45d55d51616e011be57a02c5
stubbsy2712 <53872169+stubbsy2712@users.noreply.github.com>
2026-03-15 10:58:45 +0000

    begun work on images task, slug task appears to be working

--- resources/views/components/product-grid-item.blade.php
+++ resources/views/components/product-grid-item.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('product.show', $product) }}">
-    <img src="{{ asset('images/product/' . $product->image) }}" alt="{{ $product->name }}">
+    <img src="{{ asset('images/product/' . $product->thumbnail) }}" alt="{{ $product->name }}">
     {{ $product->name }}
 </a>
