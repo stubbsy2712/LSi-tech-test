571b555fa3bb3aefc60a740fd8ca1e099ada3601
Thomas Stubbs <stubbsy2712@gmail.com>
2026-03-15 21:17:49 +0000

    re-added getRouteKeyName that vanished in previous commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,11 @@ class Product extends Model
 {
     use HasFactory;
 
+	public function getRouteKeyName()
+	{
+		return 'slug';
+	}
+
 	public function images()
 	{
 		return $this->hasMany(ProductImage::class);
